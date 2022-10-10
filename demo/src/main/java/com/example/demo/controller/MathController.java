c0ffa7131d7d3bebf18b571da6d044bc46a33de7
Giuseppe Corrao <josephcorr99@gmail.com>
2022-10-10 19:14:38 +0200

    guarda lore

--- demo/src/main/java/com/example/demo/controller/MathController.java
+++ demo/src/main/java/com/example/demo/controller/MathController.java
@@ -1,32 +1,45 @@
 package com.example.demo.controller;
 
 import com.example.demo.entities.ArithmeticOperation;
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/math")
 public class MathController {
 
     @GetMapping()
-    @ApiParam(value = "welcome msg print from MathController",required = false)
-    public String welcomeMathMsg(){
+    @ApiParam(value = "welcome msg print from MathController", required = false)
+    public String welcomeMathMsg() {
         return "welcome from MathController";
     }
+
     @GetMapping("/division-info")
-    @ApiParam(value = "print division info but return an entities",example = "ArithmeticOperation")
-    public ArithmeticOperation printDivisionInfo(){
-        return new ArithmeticOperation();
+    @ApiParam(value = "print division info but return an entities", example = "ArithmeticOperation")
+    public ArithmeticOperation printDivisionInfo() {
+
+        return new ArithmeticOperation(
+                "division",
+                2,
+                "the division are the opposite of multipl.......",
+                new String[]{"the properrties of division", "the prop of div"});
     }
 
     @PostMapping("/multiplication")
-    @ApiParam(value = "int",allowableValues = "only integer",required = true,defaultValue = "0/null")
-    public int printMultiplicationInfo(int integer,int integer2){
+    @ApiParam(value = "int", allowableValues = "only integer", required = true, defaultValue = "0/null")
+    public int printMultiplicationInfo(int integer, int integer2) {
         return integer * integer2;
     }
-    @GetMapping("square/{n}")
-    @ApiParam(value = "int",allowableValues = "only int")
-    public int printSquare(@PathVariable() int n){
-        return n;
+
+    @GetMapping("/square/{n}")
+    @ApiOperation(value = "Square", notes = "Prints the result of the square from a number")
+    public int printSquare(@PathVariable int n) {
+
+        int result = (int) Math.sqrt(n);
+
+        return result;
     }
 }
--- demo/src/main/java/com/example/demo/entities/ArithmeticOperation.java
+++ demo/src/main/java/com/example/demo/entities/ArithmeticOperation.java
@@ -1,12 +1,35 @@
 package com.example.demo.entities;
 
-import org.springframework.stereotype.Component;
 
-@Component
+
 public class ArithmeticOperation {
 
-    private String name;
-    private int minNumberOfOperands;
-    private String description;
-    private String[] properties;
+     private String name;
+     private int minNumberOfOperands;
+     private String description;
+     private String[] properties;
+
+
+    public ArithmeticOperation(String name, int minNumberOfOperands, String description, String[] properties) {
+        this.name = name;
+        this.minNumberOfOperands = minNumberOfOperands;
+        this.description = description;
+        this.properties = properties;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMinNumberOfOperands() {
+        return minNumberOfOperands;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String[] getProperties() {
+        return properties;
+    }
 }
